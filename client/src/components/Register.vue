587d2e286bc0035b9211f8960f53176ed7a87ca0
SyrahViognier <roger@lewislewiswine.com>
2019-07-08 10:19:42 +0200

    add Register.vue

---
+++ client/src/components/Register.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h1>Register</h1>
+    <input
+      type="email"
+      name="email"
+      v-model="email"
+      placeholder="email"
+    />
+    <br>
+    <input
+      type="password"
+      name="password"
+      v-model="password"
+      placeholder="password"
+    />
+    <br>
+    <button>Register</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  watch: {
+    email (value) {
+      console.log('email has changed ', value)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
